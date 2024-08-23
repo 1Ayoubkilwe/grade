4dbdb5442902f006ea1052a6a4e354c22f57931f
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-08-23 17:59:40 +0300

    my answers

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+let  grade =37
+if (grade >= 90 && grade <=100){
+    console.log("You got an A")
+}else if (grade >=75 && grade < 90){
+    console.log("You got an B")
+}else if(grade >=60 && grade <75){
+    console.log("You got an C")
+}else if(grade <60){
+    console.log("You got an FAILED")
+}
